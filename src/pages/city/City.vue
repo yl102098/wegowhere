497a67aa881b5f7f46a19b001014b75013d0e2f8
yl102098 <yangliang102098@163.com>
2021-02-20 12:23:06 +0800

    ajax city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,28 +1,52 @@
 <template>
-    <div>
-        <Header></Header>
-        <Search></Search>
-        <List></List>
-        <Alphabet></Alphabet>
-    </div>
+  <div>
+    <Header></Header>
+    <Search></Search>
+    <List :cities="cities" :hotCities="hotCities"></List>
+    <Alphabet :cities="cities"></Alphabet>
+  </div>
 </template>
 
 <script>
-    import Header from './components/Header'
-    import Search from './components/Search'
-    import List from './components/List'
-    import Alphabet from './components/Alphabet'
-    export default {
-        name:"City",
-        components:{
-            Header,
-            Search,
-            List,
-            Alphabet
-        }
-    }
+import axios from "axios";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import List from "./components/List";
+import Alphabet from "./components/Alphabet";
+export default {
+  name: "City",
+  components: {
+    Header,
+    Search,
+    List,
+    Alphabet,
+  },
+  data() {
+    return {
+      cities: {},
+      hotCities: [],
+    };
+  },
+  methods: {
+    getCityInfo() {
+      axios.get("/api/city.json").then(this.handlegetCity);
+    },
+    handlegetCity(res) {
+      // console.log(res)
+      res = res.data;
+      if (res.ret && res.data) {
+        const data = res.data;
+        this.cities = data.cities;
+        this.hotCities = data.hotCities;
+      }
+    },
+  },
+
+  mounted() {
+    this.getCityInfo();
+  },
+};
 </script>
 
 <style lang="less" scoped>
-
 </style>
--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,39 +1,35 @@
 <template>
-    <ul class="list">
-        <li class="item">A</li>
-        <li class="item">A</li>
-        <li class="item">A</li>
-        <li class="item">A</li>
-        <li class="item">A</li>
-        <li class="item">A</li>
-        <li class="item">A</li>
-        <li class="item">A</li>
-        <li class="item">A</li>
-        <li class="item">A</li>
-    </ul>
+  <ul class="list">
+    <li class="item" v-for="(item, key) of cities" :key="key">
+        {{ key }}
+    </li>
+  </ul>
 </template>
 
 <script>
-    export default {
-        name:"CityAlphabet"
-    }
+export default {
+  name: "CityAlphabet",
+  props: {
+    cities: Object,
+  },
+};
 </script>
 
 <style lang="less" scoped>
-.list{
-    display: flex;
-    flex-direction: column;         //纵向排列
-    justify-content: center;        //垂直居中
-    position: absolute;
-    top: 1.58rem;
-    right: 0;
-    bottom: 0rem;
-    width: 0.4rem;
-    // background-color: #09f;
-    .item{
-        line-height: 0.44rem;
-        text-align: center;
-        color: #00BCD4;
-    }
+.list {
+  display: flex;
+  flex-direction: column; //纵向排列
+  justify-content: center; //垂直居中
+  position: absolute;
+  top: 1.58rem;
+  right: 0;
+  bottom: 0rem;
+  width: 0.4rem;
+  // background-color: #09f;
+  .item {
+    line-height: 0.44rem;
+    text-align: center;
+    color: #00bcd4;
+  }
 }
 </style>
